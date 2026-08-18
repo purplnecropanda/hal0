fa07aed682f2c03f79a3aa14fedb87a40309285d
purplnecropanda <60634699+purplnecropanda@users.noreply.github.com>
2026-08-18 18:23:35 +0100

    add focused Hermes module evaluation check

---
+++ checks/module-hermes.nix
@@ -0,0 +1,22 @@
+{ pkgs, module }:
+let
+  system = pkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      module
+      {
+        services.hal0.enable = true;
+        services.hal0.hindsight.enable = false;
+        services.hal0.openwebui.enable = false;
+        services.hal0.benchWorker.enable = false;
+        services.hal0.comfyui.enable = false;
+      }
+    ];
+  };
+in
+pkgs.runCommand "hal0-module-hermes-check" {} ''
+  test '${system.config.services.hal0.hermes.runtime}' = native
+  test -n '${system.config.systemd.services.hal0-hermes-provision.serviceConfig.ExecStart}'
+  test -n '${system.config.systemd.services."hal0-agent@".serviceConfig.ExecStart}'
+  touch $out
+''
