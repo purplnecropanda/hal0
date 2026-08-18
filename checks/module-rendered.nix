bde4fd880e22d9155f4e832e220cb722ca130e23
purplnecropanda <60634699+purplnecropanda@users.noreply.github.com>
2026-08-18 18:23:50 +0100

    add focused rendered systemd module check

---
+++ checks/module-rendered.nix
@@ -0,0 +1,25 @@
+{ pkgs, module }:
+let
+  system = pkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      module
+      {
+        services.hal0.enable = true;
+        services.hal0.hindsight.enable = false;
+        services.hal0.openwebui.enable = false;
+        services.hal0.hermes.enable = false;
+        services.hal0.benchWorker.enable = false;
+        services.hal0.comfyui.enable = false;
+      }
+    ];
+  };
+  api = system.config.systemd.services.hal0-api.serviceConfig;
+  agent = system.config.systemd.services."hal0-agent@".serviceConfig;
+in
+pkgs.runCommand "hal0-module-rendered-check" {} ''
+  test '${api.Type}' = simple
+  test -n '${api.ExecStart}'
+  test -n '${agent.ExecStart}'
+  touch $out
+''
