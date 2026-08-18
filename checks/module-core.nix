36f94614d445ae5fa76670d559648e0f33d0b115
purplnecropanda <60634699+purplnecropanda@users.noreply.github.com>
2026-08-18 18:23:05 +0100

    add focused core module evaluation check

---
+++ checks/module-core.nix
@@ -0,0 +1,18 @@
+{ pkgs, module }:
+let
+  system = pkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      module
+      {
+        services.hal0.enable = true;
+        services.hal0.port = 8080;
+      }
+    ];
+  };
+in
+pkgs.runCommand "hal0-module-core-check" {} ''
+  test '${toString system.config.services.hal0.port}' = 8080
+  test -n '${system.config.systemd.services.hal0-api.serviceConfig.ExecStart}'
+  touch $out
+''
