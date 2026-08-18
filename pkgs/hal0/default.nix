a3cd48af694b0f061516d89804828b6ae5835518
purplnecropanda <60634699+purplnecropanda@users.noreply.github.com>
2026-08-18 18:31:07 +0100

    use explicit python3 runtime in composed package

---
+++ pkgs/hal0/default.nix
@@ -0,0 +1,56 @@
+{ lib
+, symlinkJoin
+, makeWrapper
+, coreutils
+, curl
+, git
+, jq
+, lshw
+, pciutils
+, podman
+, procps
+, sudo
+, systemd
+, util-linux
+, python3
+, hal0-core
+, hal0-ui
+, hal0-assets
+}:
+
+symlinkJoin {
+  name = "hal0";
+  paths = [ hal0-core hal0-assets ];
+
+  postBuild = ''
+    mkdir -p $out/share/hal0
+    ln -s ${hal0-ui}/dist $out/share/hal0/ui-dist
+
+    rm -f $out/bin/hal0 $out/bin/hal0-agent
+    runtimePath=${lib.makeBinPath [
+      podman sudo systemd coreutils util-linux curl jq git pciutils lshw procps python3
+    ]}
+
+    makeWrapper ${hal0-core}/bin/hal0 $out/bin/hal0 \
+      --set-default HAL0_USR_LIB "$out/usr-lib/hal0/current" \
+      --set-default HAL0_LIB "$out/usr-lib/hal0" \
+      --set-default HAL0_UI_DIST "$out/share/hal0/ui-dist" \
+      --prefix PATH : "$runtimePath"
+
+    makeWrapper ${hal0-core}/bin/hal0-agent $out/bin/hal0-agent \
+      --set-default HAL0_USR_LIB "$out/usr-lib/hal0/current" \
+      --set-default HAL0_LIB "$out/usr-lib/hal0" \
+      --set-default HAL0_UI_DIST "$out/share/hal0/ui-dist" \
+      --prefix PATH : "$runtimePath"
+  '';
+
+  passthru = { inherit hal0-core hal0-ui hal0-assets; };
+
+  meta = {
+    description = "Open-source home AI inference platform";
+    homepage = "https://hal0.dev";
+    license = lib.licenses.asl20;
+    mainProgram = "hal0";
+    platforms = lib.platforms.linux;
+  };
+}
